c9a606c62011a7a35d113b2b68ce64a938fa1243
claudito <1107000044@upci.edu.pe>
2019-04-25 20:41:16 -0500

    clase 01 02/02/ Medio Tiempo

---
+++ semana01/source.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Comentario
+#comentario
+/*
+comentario
+con salto 
+de linea
+*/
+$nombres   = "Luis Augusto";
+
+$apellidos = "Claudio Ponce";
+
+$celular   = "+51997935085";
+
+$correo    = "luis.claudio@perutec.com.pe";
+
+//echo $correo;
+
+$datos     =  array(
+
+"nombres"   =>$nombres,
+"apellidos" =>$apellidos,
+"celular"   =>$celular,
+"correo"    =>$correo
+  
+);
+
+var_dump($datos);
+
+
+ ?>
